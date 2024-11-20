bf8ae615e27251335e5755ae65fa2980bee8f9ec
Moruf Adebola <hellokmgmedia@gmail.com>
2024-11-20 09:34:50 +0100

    Yes, Arranged the Nav bar And the Landing page

--- NAVBAR/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for Hamburger Menu
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
