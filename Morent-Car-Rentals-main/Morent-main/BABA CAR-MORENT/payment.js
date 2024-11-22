7b8256a0b31abe53af8c626dccea518bae74ac76
Moruf Adebola <hellokmgmedia@gmail.com>
2024-11-22 11:46:11 +0100

    New updates on Paths!

--- Morent-Car-Rentals-main/Morent-main/BABA CAR-MORENT/payment.js
+++ Morent-Car-Rentals-main/Morent-main/BABA CAR-MORENT/payment.js
@@ -6,7 +6,9 @@ const cardType=document.getElementByld('card-type').value;
 const cardNumber=document.getElementByld('card-number').value;
 const expirationDate=document.getElementByld('expiration-date').value;
 const nameOnCard=document.getElementByld('name-on-card').value;
+
 //Validate form data
+
 if(!cardType//!cardNumber//!expirationDate/!cvv//!nameOnCard){
  ) paymentStatus.textContent="Please fill in all fields.";
  return;
